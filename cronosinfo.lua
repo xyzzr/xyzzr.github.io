52b77abeb2158672896e5eeb1258e9f5f9f875b1
xyzzr <110189904+xyzzr@users.noreply.github.com>
2024-05-22 21:34:48 +0300

    Create cronosinfo.lua

---
+++ cronosinfo.lua
@@ -0,0 +1,10 @@
+_G.remote_One = 1
+_G.remote_Two = 2
+_G.remote_Three = 3
+_G.remote_Sex = 4
+_G.remote_Sigma = 5
+_G.remote_Idkhowtocount = 4308660407
+_G.remote_Divorced = 7
+_G.remote_Ligma = 8
+_G.remote_Family = 9
+_G.remote_Nuking = 10
