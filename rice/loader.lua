08b73b80afd0cdec09b4767b7efa948d0d1eebe3
xyzzr <110189904+xyzzr@users.noreply.github.com>
2024-06-25 21:00:12 +0300

    Update loader.lua

--- rice/loader.lua
+++ rice/loader.lua
@@ -4,6 +4,18 @@ OMG NO RATS!?
 NO LOGGERS!?
 WHATTTT THIS IS UNHEARD OF!
 
+Supported games reminder 
+https://www.roblox.com/games/9043532917/Ramp-Jumping-On-sports-cars
+https://www.roblox.com/games/6816975827/Car-Suspension-Test
+https://www.roblox.com/games/4456880028/Ro-Crash
+https://www.roblox.com/games/5752297721/Z-Bus
+https://www.roblox.com/games/7764872557/UPDATE-Realistic-Car-Crash-Simulator
+https://www.roblox.com/games/3239849876/TRAFFIC-Road-of-Car-Crash
+https://www.roblox.com/games/7720943627/Car-Crash-Test
+https://www.roblox.com/games/10592754185/BT-Drive-BETA
+https://www.roblox.com/games/4643061038/Car-Crash-Test
+
+
 CHANGELOG
 
 6/25/2024: 5:32PM
